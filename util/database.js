75f94c06f7e1e036fbfc81f5d3c01087fe4143e3
Prakhar Sharma <you@example.com>
2019-03-29 15:27:00 +0530

    remaining changes to database file

--- util/database.js
+++ util/database.js
@@ -3,10 +3,10 @@ const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = callback => {
 
-MongoClient.connect('mongodb+srv://prakhar_sharma:PSnaeT3iHcHvT1PX@cluster0-db0aq.mongodb.net/test?retryWrites=true')
+MongoClient.connect('mongodb+srv://prakhar_sharma:PSnaeT3iHcHvT1PX@cluster0-db0aq.mongodb.net/test?retryWrites=true', {useNewUrlParser: true})
 .then((result) => {
 console.log('Conected!');
-cb(result);
+callback(result);
 })
 .catch((err) => {
 console.log(err);
